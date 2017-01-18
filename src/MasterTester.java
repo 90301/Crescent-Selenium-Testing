041a176d9fc7ea7fad8a33b49790ea5cc71ab007
Mark Layton <iris4@live.com>
2017-01-17 21:47:24 -0500

    Started Repository

---
+++ src/MasterTester.java
@@ -0,0 +1,13 @@
+
+public class MasterTester {
+
+	/**
+	 * This method runs when the program starts.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Put Test Code in Here?
+
+	}
+
+}
